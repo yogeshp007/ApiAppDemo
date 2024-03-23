db5a4fbabe95f5b7b0be582a181dd8a32208685a
yog-pande <70682331+yog-pande@users.noreply.github.com>
2024-03-23 08:25:28 +0530

    crud with help of api

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -33,6 +34,66 @@ namespace ApiAppDemo.Controllers
             }
 
             return View(list);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(UserDetail model)
+        {
+            string data = JsonConvert.SerializeObject(model);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+
+            var response = client.PostAsync(client.BaseAddress + "/demo/AddUser", content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            UserDetail user = new UserDetail();
+
+            var response = client.GetAsync(client.BaseAddress + "/demo/GetUserById/" + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var data = response.Content.ReadAsStringAsync().Result;
+                user = JsonConvert.DeserializeObject<UserDetail>(data);
+            }
+
+            return View("Create", user);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(UserDetail model)
+        {
+            string data = JsonConvert.SerializeObject(model);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+
+            var response = client.PutAsync(client.BaseAddress + "/demo/UpdateUser/" + model.Id, content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {            
+            var response = client.DeleteAsync(client.BaseAddress + "/demo/DeleteUser/"+id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View("Index");
         }
     }
 }
